ca3dfb134af3010de44be26974839c4ab2c5a75d
jituman01 <naimulislamjitu585@gmail.com>
2026-06-13 15:35:55 +0600

    added statistics and testimonial section

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,13 +1,23 @@
 import WhyChoose from "@/components/WhyChoose";
 import Hero from "../components/Hero";
+import Statistics from "@/components/Statistics";
+import Testimonials from "@/components/Testimonials";
 
 export default async function HomePage() {
+
+  const stats = {
+    totalEvents: 30,
+    totalAttendees: 4000,
+    totalOrgs: 10
+  }
 
 
   return (
     <div>
       <Hero />
-      <WhyChoose/>
+      <WhyChoose />
+      <Statistics stats={stats} />
+      <Testimonials/>
     </div>
   );
 }
